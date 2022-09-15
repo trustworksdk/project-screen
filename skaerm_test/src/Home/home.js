01d10d4e4292e7936b2fc4a96703bc5b01d39031
dittehjorth <99872686+dittehjorth@users.noreply.github.com>
2022-09-15 12:35:42 +0200

    hej

--- skaerm_test/src/Home/home.js
+++ skaerm_test/src/Home/home.js
@@ -1,23 +1,48 @@
-import axios from "axios"
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+// https://api.trustworks.dk/knowledge/projects
+
+
 
 const Home = () => {
 
+    const token = "xxx"
+
+    const [project, setProject] = useState();
+    const [projectData, setProjectData] = useState();
+
+    const config = {
+        headers: { Authorization: `Bearer ${token}`}
+    };
+
+   
     const getProjects = () => {
-        axios.get('https://api.trustworks.dk/knowledge/projects')
+        axios.get('https://jsonplaceholder.typicode.com/todos/1', config)
         .then(response => {
-            console.log(response)
+            setProject(() => response.data)
+            console.log("Projects:", project)
         }).catch(error => {
             console.log(error)
         })
     }
 
+    useEffect(() => {
+        setProjectData(project);
+        // console.log(projectData);
+    }, [project]);
 
+
+    const getProjectName = () => {
+        console.log("Project name:", project.title )
+    }
 
     return (
         <div>
             <h1>This is home</h1>
             <h3>Test af API kald: </h3>
             <button onClick={getProjects} >Get projects</button>
+            <button onClick={getProjectName} >Get project name</button>
 
         </div>
     )
