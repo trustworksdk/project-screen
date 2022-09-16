77cbb9ebd4630f59817012498eb6c53cc0d4ce5b
dittehjorth <99872686+dittehjorth@users.noreply.github.com>
2022-09-16 11:17:26 +0200

    .

--- screen/src/Home/home.js
+++ screen/src/Home/home.js
@@ -6,17 +6,27 @@ import { Card } from "react-bootstrap";
 
 const Home = () => {
 
-    const token = "eyJraWQiOiIvcHJpdmF0ZUtleS5wZW0iLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczovL3RydXN0d29ya3MuZGsiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJkaXR0ZS5oam9ydGgiLCJpYXQiOjE2NjMyNTA2MTcsImV4cCI6MTY2MzI4NjYxNywiZ3JvdXBzIjpbIlVTRVIiXSwianRpIjoiNWU0ZDY0OWItOWMzNC00NGEyLTk3NWItZjA0ZGEzMjBiOGM1In0.nlwQEUKXRQ61N0WbQ_93R9DN33z8WAdKAfaAvotsfZmD8XeGfgeU1jQM5fIipBNBgWjw5WY_bHCmZGmu9JzGJNo66t6dDNdySrYAU5_87GKihlOn_cva2UsVqiFNFq57qWMAN31ultQk3dWLdxZJJrzFuLUUX2KiXQyS0hPtc2Vu2gZkfPjFVxbh75dUIs6o9mL8NQ23EzqcawZLUVk7ivod-35RKc26FK2TePqqoUKdTmH6xpAXk_GySKJa5HiBOWp48H-fryUn0U3DGC2UaWldfO3vSE6JUqNgV83qNOuUm7k4yQkyQ0JBm7EwMp-SktjSAniFr_FSvR-Zx_8txRN23gcAjnXt1zpSclQMtD-nDOoVcqc6l2F7GQtq2W34CkyV2zBCrvMeh5Jxdhcpzkf2CnhhezgzXsm12yJDtmwvFhOz40Cr83NPG0D5SKCz2ruzOohWJfTMABVRTQ08QVsi8p492_EvXQ4R2TSwgjY7gqraf86eBKVNMm8YRTwrOeItxk6k8ByF8dAW-JhIcAnXp8xbi65KWaerwdb-6CMVSxHw0WrGbC67ylD3LLsHb07R2zEaS3PHANo98KPPqlIwlaWZV1_9fCWbXQn_1E_Qgn-CXDZ1veaKcefpYoco64uihmYB2tgYFxm1jH0Hxc-rulVGv6NuRPKHPguhCLM";
-
-    const [projects, setProjects] = useState();
-    const [projectData, setProjectData] = useState();
+    const token = "eyJraWQiOiIvcHJpdmF0ZUtleS5wZW0iLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczovL3RydXN0d29ya3MuZGsiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJkaXR0ZS5oam9ydGgiLCJpYXQiOjE2NjMzMTg1NTEsImV4cCI6MTY2MzM1NDU1MSwiZ3JvdXBzIjpbIlVTRVIiXSwianRpIjoiNmE0ZTdkZDMtMGQ1ZC00OWUzLWEzNmYtODc5Njk1ZThjNTc1In0.GRP--WphWfJ4v7a_WjK5kw4Y_ZAdIl6waFTX61fwhS4Jj_OMxWXKLTZmeVxl8yAH1ZG5vNFWaB6wuV40YlWk3SBKamy3Zcx23XHk3nvB-wXlpQVskFCzLOigLjB7erslgUWUEbZvzFz-xAyroKY5zpxUi_P0yzOULmK4jIED4IAICH6VVrPDpLHTE2d_5oFFyUePKUfGTrL2KjT0Ka4r-m0uL0d0vmcvtq-gVcRuKEeev-NEKQoyP6jI9aO7vyOLIYe0jzpOizeKS5r8c6U7rYo04st1lzde7SzhNvSNftiBijdkZF2djaqlcrBtNbTkazqcxC0qYsMQ_-GwmZPk_ymjCvgKWskBN02H-gTPe7XNIiHJeiOf6qEQhH1NykgfpTQetfhxhKa4vKEOcUsux_MGwNbC3nRrQKVdZvj360oQ5nBRo8JHjiPh94L5SvZQYIXtlspQOEwDAIRZaQ7cBe94jRsFphUoXqTUckOFkpferVVz9zqZ54I3KkuR6HxmRsxd_HhFKz1KWDJylErYIHyzEK3ytg_QZb7HSh1iPEo3_NdJGaG1x4NXTPNcqGx-rg3_F8jzVnikiKkx7MVf3t-jT_x8Ldhc4yM-2iofciAKo2cpoVHBMs-C3ivvQy1NMkK0vi0fGe-t75gsDNhyiIEgBkqO8pDPitGNUyQ_RxM";
 
     const config = {
         headers: { Authorization: `Bearer ${token}`}
     };
 
+    const [projects, setProjects] = useState();
+    const [projectData, setProjectData] = useState();
+
+    useEffect(() => {
+        axios.get('https://api.trustworks.dk/knowledge/projects', config)
+        .then(response => {
+            setProjects(() => response.data)
+            console.log("Projects:", projects)
+        }).catch(error => {
+            console.log(error)
+        });
+    }, []);
+
    
-    const getProjects = () => {
+    const printProjectsToConsole = () => {
         axios.get('https://api.trustworks.dk/knowledge/projects', config)
         .then(response => {
             setProjects(() => response.data)
@@ -28,7 +38,7 @@ const Home = () => {
 
     useEffect(() => {
         setProjectData(projects);
-        console.log(projectData);
+        // console.log("projectData:", projectData);
     }, [projects]);
 
 
@@ -40,8 +50,8 @@ const Home = () => {
         <div>
             <h1>This is home</h1>
             <h3>Test af API kald: </h3>
-            <button onClick={getProjects} >Get projects</button>
-            <button onClick={getProjectName} >Get project name</button>
+            <button onClick={printProjectsToConsole} >Print projects</button>
+            {/* <button onClick={getProjectName} >Get project name</button> */}
             <br/>
             <br/>
             <h2> Projekter: </h2>
