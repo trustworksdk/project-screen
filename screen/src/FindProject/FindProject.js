9860e539607e4b2f53a8957f782714ec3a6bd754
dittehjorth <99872686+dittehjorth@users.noreply.github.com>
2023-03-03 11:15:10 +0100

    hej

--- screen/src/FindProject/FindProject.js
+++ screen/src/FindProject/FindProject.js
@@ -1,26 +1,20 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Card, Button, Row, Col } from "react-bootstrap";
+import { Card, Button, Carousel } from "react-bootstrap";
 import { Wrapper } from "./FindProject.styles";
 import { useNavigate } from "react-router-dom";
 import { Base64 } from "js-base64";
-import Project from "../Project";
-import kurt from "../img/kurt.jpg";
+import kurt from "../img/kurt.jpg"
 
 
 const FindProject = () => {
 
     const navigate = useNavigate();
 
-    const token = "eyJraWQiOiIvcHJpdmF0ZUtleS5wZW0iLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczovL3RydXN0d29ya3MuZGsiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJkaXR0ZS5oam9ydGgiLCJpYXQiOjE2NzY5MTIyNzksImV4cCI6MTY3Njk0ODI3OSwiZ3JvdXBzIjpbIlVTRVIiXSwianRpIjoiMDY4ZDRkNTctNTMwMi00NWJmLTgyZmMtNjE2NmE5NzY4NDc4In0.eoXxFlZWl_rz-LGT1pFzMtmc-rICbgnt4UBML0rksqJUpETS8_fQXDw5UiB0SsMhiR5iMuyVm12NBliLkKptrb5GwyZ-kIsab7rYuXhEGBAuVjmgerI9-cqo7UuDFc5CHNK4nqOLqe_Xv_wvajLNDOO4vDmio7PxFdER4TGb0l42lSO6HrG5VvjFmPSL6LjrmzXZ8omU1xOiJlGjoj_plCf6AG8TRYYIweHMDaxsipXWJqzgYZ_7jPIVE6KyGlgKJneQ2kiLTKmW0tDdJTBL0JXMEsmBk14h7c-rjFAP9pdXP0DbsWm0eyxmd2rAmqz2RrTeVIoPpksi4QDvfkuJEa86fJZvg2KJ-zd6e1SBpSsXjrWq9PTUTmlZvwOeMMzbT1qcoOoi5bAm2nKs_UGjfigrFuKFYFE8hiT-sPUhw_kT481ZbpA3fp9nmTWek0IUE3aVr1AyiiddPbrsf58_euBMYIgNALuXEBAjw3jKGs3beUCo2ZlZHJQfGfDCo4vdwHhCUYj-ls3wUUJ4XT6-rV2oChj-aABEuTXeUojFBtY7p7ZtrjBAF8HKA7sNlD70F0tQr-PKHROYZqHJ7Ee69gba-x_LCGzFn5nfhgF3eTrfi-xvq-ETtT8l9WcfHRDdCzNtr7sjNCRc-bceRc66qS5WdDosAy64xzhBUpRBwGg";
+    const token = "eyJraWQiOiIvcHJpdmF0ZUtleS5wZW0iLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczovL3RydXN0d29ya3MuZGsiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJkaXR0ZS5oam9ydGgiLCJpYXQiOjE2Nzc4MzYwNDUsImV4cCI6MTY3Nzg3MjA0NSwiZ3JvdXBzIjpbIlVTRVIiXSwianRpIjoiYjZhNDZjNWEtZTRiMi00NDllLThiYzItOTgxMmNkNTdhNGNhIn0.XmVmDetMaK6LGt3Qq2_Uzdl-Z6DAbQoPnqQ_c4W640AKgGRgk69UuWvtTMeKZYKZTlIPfu5AGT3cbpa5kCR_mk-qNOutZvNOgB1Wpj_wwd4hqvn9IHCBkeZYOaruX1V-_clLOpkFFuK1b-y9wzo9KUWicEr50Nzkdm98C2Op0wzaE0yhQXn2sa0b6YK51SsVv5BYGXQQM2yIPeNILlZx6guE_0GvwpqjS7sBWLp9r7_6H93SPkn_pWofh_y-5YFRXsM8FfarSyLztqjZPYoCbuMVrLOUYnRGvYBFkQbMkseE8uIUjwKvJvykf5_GZGNoousUCQou4Hk3BaT4A30vRLtEBcIEsx6Ar7yAvoQXV0xf3mCDDjSjBqfwCtLUaWgaG6OaeLNheFL8HrVqNnBXAf7U1HXlOeKl0ATFM2rZ0gaZSbVfs7c7cfX9vbSar4Rv2--zFSENhtMdC8l6VxKbb4YSn9suKFb48zZ74RVAgGP4zX54mYpTNbQ3uNBX9RroGcge8glATvmSFfycZFBSRlhTdbgxdUAQZg1EeDCRI69UuEegwgJRQbs7_qaR-umh8ebnIRUqwjz5iHvqzWwuA3BK3AYih9cye4VCROCPxKMF5E-tLTC2o2KOxu9SdAkBxyONM9kInyxA21MNDZZuGRLK0UdsXb0uzkQJbWfhuS0";
     const config = { headers: { Authorization: `Bearer ${token}`} };
 
     const [projects, setProjects] = useState();
-    
-    const [clientData, setClientData] = useState();
-    const [clientID, setClientID] = useState();
-    const [clientLogo, setClientLogo] = useState();
-
     const [employeeList, setEmployeeList] = useState([]);
     const [clientList, setClientList] = useState([]);
    
@@ -30,7 +24,7 @@ const FindProject = () => {
         axios.get('https://api.trustworks.dk/knowledge/projects', config)
         .then(response => {
             setProjects(() => response.data)
-            // console.log("Projects:", projects)   
+            console.log("Projects:", projects)   
         }).catch(error => {
             console.log(error)
         });
@@ -43,33 +37,20 @@ const FindProject = () => {
                 axios.get(`https://api.trustworks.dk/users/${user.useruuid}/photo`, config).then(response => { 
                     // setEmployeePhoto(response.data.file);
                     const photo = response.data.file;
-                    setEmployeeList(employeeList => [...employeeList, {id: user.useruuid, file: photo}])
-                    
-                    
+                    setEmployeeList(employeeList => [...employeeList, {id: user.useruuid, file: photo}])  
                 }).catch(error => {
                     console.log(error)
-                })
-                
+                }) 
             })
         })
         // setEmployeeList(employeeList.filter( (ele, ind) => ind === employeeList.findIndex( elem => elem.id === ele.id && elem.file === ele.file)))
-        // console.log(employeeList)
     }, [projects]);
-
-
-    function handleEmployeeList() {
-        // let pp = employeeList.filter( (ele, ind) => ind === employeeList.findIndex( elem => elem.useruuid === ele.useruuid && elem.file === ele.file))
-        // setEmployeeList(pp)
-        // console.log(pp)
-    }
 
 
     function getEmployee(props) {
         const foundItem = employeeList.find(item => item.id === props);
-        // console.log(foundItem)
         return foundItem ? foundItem.file : null;
     }
-
 
 
     useEffect(() => {
@@ -90,33 +71,68 @@ const FindProject = () => {
     }
 
 
-
-
-
-
-
-
     return (
         <Wrapper>
 
-            {/* {handleEmployeeList()} */}
-           
-
-
             <h1>This is FindProject</h1>
-            <br/>
             <br/>
             <Button onClick={() => navigate('/')}> Tilbage til home </Button>
             <br/>
-            <br/>
-    
 
+            {/* <Carousel>
+                <Carousel.Item> 
+                    <img src={kurt} alt="First slide" />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item> 
+                    <img src={kurt} alt="First slide" />
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel> */}
+  
             <div>
+
+
+
+            <Carousel >
+            {projects?.map(project => (
+            <Carousel.Item  >
+                
+                    <img src={`data:image/jpeg;base64,${ getClientLogo(project.clientuuid) }`} style={{ width: 540, height: 230, borderRadius: '3%' }}/>
+            
+                    <Carousel.Caption> 
+                        <h1> {project.name} </h1> 
+                        <p> {project.description} </p> 
+                        <b> Trustworkere på projektet: </b>
+
+                        <p>                        
+                        { project.projectDescriptionUserList?.map(user => (
+                            <div>
+                                 {<img src={`data:image/jpeg;base64,${ getEmployee(user.useruuid) }`} style={{ width: 140, height: 130, borderRadius: '50%' }}  />}
+                                <div> {user.description } </div>
+                            </div>
+                        )) }
+                        </p>
+                    </Carousel.Caption>
+                    
+                
+            </Carousel.Item>
+            ))}
+            </Carousel>
+            
+            
+
+
+
             {projects?.map(project => (
             <Card  >
                 <Card.Body>
-                    {/* {getClientLogo(project.clientuuid)} */}
-                    {/* <Card.Img  /> */}
                     <img src={`data:image/jpeg;base64,${ getClientLogo(project.clientuuid) }`} style={{ width: 540, height: 230, borderRadius: '3%' }}/>
             
                     <Card.Title className="cardtitle" > {project.name} </Card.Title>
@@ -136,6 +152,9 @@ const FindProject = () => {
                 </Card.Body>
             </Card>
             ))}
+            
+
+            
             </div>
         </Wrapper>
     )
