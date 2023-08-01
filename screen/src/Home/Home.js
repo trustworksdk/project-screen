25745766d3ad3376978e1f63906b5457c6e4d645
dittehjorth <99872686+dittehjorth@users.noreply.github.com>
2023-08-01 14:19:55 +0200

    redesign af home

--- screen/src/Home/Home.js
+++ screen/src/Home/Home.js
@@ -1,11 +1,9 @@
 import { Wrapper } from "./Home.styles";
-import dsb from "../img/dsb2.png"
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Card, Button, CardGroup, Carousel } from "react-bootstrap";
+import { Card, Button, CardGroup, Carousel, Row, Col } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { Base64 } from "js-base64";
-import kurt from "../img/kurt.jpg"
 import {config} from "../Components/API";
 
  
@@ -18,17 +16,17 @@ const Home = () => {
     const [clientList, setClientList] = useState([]);
     
 
+    // Setting projects
     useEffect(() => {
         axios.get('https://api.trustworks.dk/knowledge/projects', config)
         .then(response => {
-            setProjects(() => response.data)
-            // console.log("Projects:", projects)   
+            setProjects(() => response.data) 
         }).catch(error => {
             console.log(error)
         });
     }, []);
 
-    
+    // Making employee list consisting of id and photo file
     useEffect(() => {
         projects?.map(project => {
             project.projectDescriptionUserList?.map(user => {
@@ -41,16 +39,17 @@ const Home = () => {
                 }) 
             })
         })
-        // setEmployeeList(employeeList.filter( (ele, ind) => ind === employeeList.findIndex( elem => elem.id === ele.id && elem.file === ele.file)))
     }, [projects]);
 
 
+    // Function to get the employee photo
     function getEmployee(props) {
         const foundItem = employeeList.find(item => item.id === props);
         return foundItem ? foundItem.file : null;
     }
 
 
+    // Making client list consisting of id and photo file
     useEffect(() => {
         projects?.map(project => {
             axios.get(`https://api.trustworks.dk/files/photos/${project.clientuuid}`, config)
@@ -63,6 +62,7 @@ const Home = () => {
     }, [projects]);
 
 
+    // Function to get the client logo
     function getClientLogo(props) {
         const foundItem = clientList.find(item => item.id === props);
         return foundItem ? foundItem.file : null;
@@ -71,7 +71,7 @@ const Home = () => {
 
     return (
         <Wrapper className="body::before" >
-        <Button onClick={() => navigate('/findproject')} > Flere projekter</Button>
+        <Button  onClick={() => navigate('/findproject')} > ＜ Flere projekter</Button>
 
 
         <Carousel className="carousell" >
@@ -81,21 +81,29 @@ const Home = () => {
 
                     <CardGroup>
                     <div className="col-sm-4">
+                    <div className="seperator">
                         <Card className="card bg-transparent border-0" >
-                            <Card.Img class="mx-auto" variant="top" src={`data:image/jpeg;base64,${ getClientLogo(project.clientuuid) }`} width={270} height={150} style={{ borderRadius: '3%' }}/>
-                            <Card.Body className="people" >
-                                { project.projectDescriptionUserList?.map(user => (
-                                    <Card className="person card bg-transparent border-0" >
-                                        <Card.Body className="personcardbody">
-
-                                        <Card.Img className="person card image" src={`data:image/jpeg;base64,${ getEmployee(user.useruuid) }`}  style={{ width: 140, height: 120, borderRadius: '50%', float: "left" }}></Card.Img>
-                                        <Card.Text className="userdescription"> {user.description}  </Card.Text>
-                                        
-                                         </Card.Body>
-                                    </Card>
-                                )) }
-                            </Card.Body>
+                            <Card.Img class="mx-auto" variant="top" src={`data:image/jpeg;base64,${ getClientLogo(project.clientuuid) }`} width={400} height={200} style={{ borderRadius: '3%' }}/>
                         </Card>
+                        <div className="ydelser-og-tools">
+                        <Card className="ydelser bg-transparent border-0">
+                                <Card.Title className="ydelser-og-tools-overskrift" > Ydelser </Card.Title>
+                                <Card.Text> 
+                                <button className="ydelser-og-tools-knap">Ydelse1</button>
+                                <button className="ydelser-og-tools-knap">Ydelse2</button>
+                                <button className="ydelser-og-tools-knap">Ydelse3</button>
+                            </Card.Text>
+                            </Card>
+                        <Card className="tools bg-transparent border-0">
+                            <Card.Title className="ydelser-og-tools-overskrift" > Tools </Card.Title>
+                            <Card.Text> 
+                                <button className="ydelser-og-tools-knap">Tool1</button>
+                                <button className="ydelser-og-tools-knap">Tool2</button>
+                                <button className="ydelser-og-tools-knap">Tool3</button>
+                            </Card.Text>
+                        </Card>
+                        </div>
+                    </div>
                     </div>
 
                     <div className="col-sm-8" >
@@ -104,137 +112,26 @@ const Home = () => {
                                 <Card.Title>
                                     <h1> {project.name} </h1>
                                     <br></br>
-                                    <h2> {project.description} </h2>
+                                    <h2 className="periode">Periode fra - periode til </h2>
+                                    <h2 className="beskrivelse"> {project.description} </h2>
                                 </Card.Title>
+
+                                <Row className="employeerow" >
+                                { project.projectDescriptionUserList?.map(user => (
+                                    <Col className="employeecol" >
+                                    <img className="employeephoto" src={`data:image/jpeg;base64,${ getEmployee(user.useruuid) }`}  />
+                                    </Col>
+                                )) }
+                                </Row>
                             </Card.Body>
                         </Card>
-
-
-                        <CardGroup className="ydelser-og-tools ">
-                            <Card className="ydelser bg-transparent border-0">
-                                <Card.Title> <h2>Ydelser</h2> </Card.Title>
-                                <Card.Text> hej </Card.Text>
-                            </Card>
-                            <Card className="tools bg-transparent border-0">
-                                <Card.Title> <h2>Tools</h2> </Card.Title>
-                                <Card.Text> hej </Card.Text>
-                            </Card>
-                        </CardGroup>
-
-
-
-
                     </div>
-
-
 
                     </CardGroup>
                 </Carousel.Item>
             ))}
         </Carousel>
-
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-
-
-
-
-
-            {/* <div className="row">
-            <CardGroup >
-            <div className="col-sm-4">
-                <Card  className="card bg-transparent border-0" >
-                    <Card.Img class="mx-auto" variant="top" src={dsb} width={170} height={150} ></Card.Img>
-                    <Card.Body className="people">
-
-                        <Card className="person card bg-transparent border-0">
-                            <Card.Body>
-                            <Card.Img className="person card image" src={kurt} style={{ width: 140, height: 120, borderRadius: '50%', float: "left" }}  ></Card.Img>
-                            <div className="person card text bg-transparent border-0" > Løsningsarkitekt- Hjælper med tekniks projektledelse, konceptualisering samt roadmaps. </div>
-                            </Card.Body>
-                        </Card>
-                        <Card className="person card bg-transparent border-0">
-                            <Card.Body>
-                            <Card.Img className="person card image" src={kurt} style={{ width: 140, height: 120, borderRadius: '50%', float: "left" }}  ></Card.Img>
-                            <div className="person card text bg-transparent border-0" > Løsningsarkitekt- Hjælper med tekniks projektledelse, konceptualisering samt roadmaps. </div>
-                            </Card.Body>
-                        </Card>
-                        <Card className="person card bg-transparent border-0">
-                            <Card.Body>
-                            <Card.Img className="person card image" src={kurt} style={{ width: 140, height: 120, borderRadius: '50%', float: "left" }}  ></Card.Img>
-                            <div className="person card text bg-transparent border-0" > Løsningsarkitekt- Hjælper med tekniks projektledelse, konceptualisering samt roadmaps. </div>
-                            </Card.Body>
-                        </Card>  
-                    </Card.Body>
-                </Card>
-
-                </div>
-                <div className="col-sm-8">
-                <Card className="card bg-transparent border-0">
-                    <Card.Body>
-                        <Card.Title> 
-                            <h1> Projektnavn </h1> 
-                            <h2> Periode for projektet </h2>
-                        </Card.Title>
-                        <br></br>
-                        <Card.Text> Signaldriften. Kortlægningen er en viderebygning på den prototype Press og Stefanlavede tidligere i 2020. Projekter berørte følgende snitflader. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </Card.Text>
-                    </Card.Body>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                
-
-                    <CardGroup className="ydelser-og-tools">
-                        <Card className="ydelser2">
-                            <Card.Title> <h2>Ydelser</h2> </Card.Title>
-                            <Card.Text> hej </Card.Text>
-                        </Card>
-                        <Card>
-                            <Card.Title> <h2>Tools</h2> </Card.Title>
-                            <Card.Text> hej </Card.Text>
-                        </Card>
-                    </CardGroup>
-
-       
-                </Card>
-
-
-           
-                </div>
-
-            
-            </CardGroup>
-            </div> */}
-
-
-            {/* {projects?.map(project => (
-            <Card  >
-                <Card.Body>
-                    <img src={`data:image/jpeg;base64,${ getClientLogo(project.clientuuid) }`} style={{ width: 540, height: 230, borderRadius: '3%' }}/>
-            
-                    <Card.Title className="cardtitle" > {project.name} </Card.Title>
-                    <Card.Text className="cardtext" >
-                        <div>  {project.description} </div>
-                        <br></br>
-                        <br></br>
-                        <b> Trustworkere på projektet: </b>
-                        
-                        { project.projectDescriptionUserList?.map(user => (
-                            <div>
-                                 {<img src={`data:image/jpeg;base64,${ getEmployee(user.useruuid) }`} style={{ width: 140, height: 130, borderRadius: '50%' }}  />}
-                                <div> {user.description } </div>
-                            </div>
-                        )) }
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-            ))} */}
-
-        
-        </Wrapper>
-        
+        </Wrapper> 
     )
 }
 
--- screen/src/Home/Home.styles.js
+++ screen/src/Home/Home.styles.js
@@ -2,27 +2,32 @@ import styled from "styled-components"
 import twlogo from "../../src/img/iconlogo_white.png";
 
 export const Wrapper = styled.div`
-    // background: #203369;
-    font-family: 'Source Sans';
-    font-size: 23px;
-    color: white;
+    // background: white;
+    font-family: "Roboto", sans-serif;
+    font-size: 20px;
+    
+  
 
 
     h1 {
-        color: #53D49C;
-        font-size: 45px;
+        color: #192434;
+        font-size: 48px;
         font-weight: bold;
     }
 
     h2 {
-        color: #53D49C;
+        color: #192434;
         font-size: 23px;
-        font-weight: bold;
     }
 
+    .periode{
+        font-weight: bold;
+        font-size: 30px;
+        margin-bottom: 15px;
+    }
    
     .card-group{
-        margin-top: 120px;
+        margin-top: 50px;
     }
 
     .row{
@@ -30,25 +35,24 @@ export const Wrapper = styled.div`
     }
 
     .col-sm-8{
-        padding-left: 150px;
-        padding-right: 150px;
-        
+        padding-left: 100px;
+        padding-right: 200px;
     }
 
 
     .col-sm-4 {
-        background-color: #CAE2E1;
-        border-radius: 0px 90px 90px 0px;
+        // background-color: #CAE2E1;
+        // border-radius: 0px 90px 90px 0px;
         padding-top: 40px;
         padding-left: 10px;
-        height: 800px;
+        height: 700px;
+        border-right: 15px solid #193B670D;
     }
 
     .card-img-top {
         width: 20%;
         height: 10vw;
         object-fit: cover;
-        
     }
 
     .people {
@@ -61,28 +65,49 @@ export const Wrapper = styled.div`
     }
 
     .btn.btn-primary {
-        margin: 10px;
-        background: #53D49C;
+        margin-top: 50px;
+        margin-left: 50px;
+        background: #DFD8D282;
         border: 0;
-        color: #16215B;
+        color: #455977;
+        font-weight: bold;
+        font-size: 20px;
+        padding: 15px;
     }
     
 
     .ydelser-og-tools{
-        margin-top: 300px;
-        padding-left: 30px;  
+        margin-top: 20%;
+        padding-left: 10%; 
+        color: #192434;
     }
 
-
-    .carousell{
-        height: 1000px;
+    .tools{
+        margin-top: 10%;
     }
 
-
-    .personcardbody{
-        display: table;
-        // text-align: center;
+    .ydelser-og-tools-overskrift{
+        font-weight: bold;
+        color: #192434;
+        font-size: 30px;
     }
+
+    .ydelser-og-tools-knap{
+        border-radius: 11px;
+        border: none;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        padding-left: 10px;
+        padding-right: 10px;
+        margin-right: 25px;
+        color: #FF7201;
+        margin-top: 10px;
+    }
+
+    // .personcardbody{
+    //     display: table;
+    //     // text-align: center;
+    // }
 
     .userdescription{
         font-size: 18px;
@@ -90,35 +115,58 @@ export const Wrapper = styled.div`
         display: flex;
         display: table-cell;
         vertical-align: middle;
-        
-    
-      
-
     }
-  
-  
-   
 
+    .employeerow {
+        margin-right: 0;
+        overflow: hidden; /* Prevent horizontal scroll if images exceed the container width */
+    }
+      
+    .employeecol {
+        padding: 0;
+        margin-right: -10;
+        // margin-bottom: 10px; /* Optional: Add some margin between each image */
+        display: inline-block;
+    } 
+      
+    .employeephoto {
+        width: 130px;
+        height: 110px;
+        border-radius: 50%;
+        margin-top: 100px;
+    }
 
-
-   
-   
-
+    .carousel-control-prev-icon, .carousel-control-next-icon {
+        position: absolute;
+        top: 40%;
+        transform: translateY(-50%);
+        // background-color: red;
+        border: 1px solid black;
+        filter: invert(100%);
+    }
+       
+    .carousel-control-prev-icon {
+        left: 7%;
+    }
     
+    .carousel-control-next {
+        right: -5%;
+    }
 
-   
+    .carousel-indicators {
+        // background-color: darkgray;
+        filter: invert(100%);
+    }
+
+    .carousel-inner{
+        height: 900px;
+    }
+
+    .carousel-control-prev{
+        width: 10px;
+    }
 
 
-  
-
-    
-
-   
-
-   
-
-  
-    
 
 
   
